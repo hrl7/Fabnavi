03d3ea8ccc230be76723ab3c1ccb99accfa7edb4
hrl7 <hrlclb@gmail.com>
2014-06-23 21:59:03 +0900

    Add register page

--- app/controllers/author.rb
+++ app/controllers/author.rb
@@ -3,8 +3,15 @@ Gdworker::App.controllers :author do
     render :register
   end
 
-  post '/register' do
-    
+  post '/create' do
+    a = Author.new
+    a.name = params[:username]
+    a.email = session["email"]
+    if a.save then 
+      return "ok"
+    else 
+      return [params,session].to_json
+    end  
   end
 
 end
