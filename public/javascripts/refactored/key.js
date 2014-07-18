3bf14c095544120b42ec6aec27c3c05cd3aa034f
hrl7 <hrlclb@gmail.com>
2014-07-18 22:12:33 +0900

    Enable to connect server

--- public/javascripts/refactored/key.js
+++ public/javascripts/refactored/key.js
@@ -1,4 +1,5 @@
 var KeyBind = function () {
+
   var keyMap = []
   ;
 
---
+++ public/javascripts/refactored/server.js
@@ -0,0 +1,29 @@
+var Server = function (){
+
+  function setThumbnail (index){
+    $.post("/project/setThumbnail",
+      {
+        project_id:Detal.projectName(),
+        author:Detail.author(),
+        thumbnail:index
+      },
+      function(){},
+      "jsonp");
+  }
+
+  function postPlaylist (){
+    $.post("/project/postConfig",
+      {
+        project_id:Detail.projectName(),
+        data:JSON.stringify(ImageList.list()),
+        author:Detail.author()
+      },
+      function(){},
+      "json");
+  }
+
+  return {
+    setThumbnail:setThumbnail,
+    postPlaylist:postPlaylist,
+  };
+}();
