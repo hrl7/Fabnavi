7f5b9be3586a6d1deb6dbbdaf86b9e10161faae1
Kentaro Teramoto <hrlclb@gmail.com>
2015-05-20 13:43:08 +0900

    Add authentication on photo upload

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,6 @@
 class PhotosController < ApplicationController
   before_action :set_photo, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate!
+  before_action :authenticate!, ohly: [:create,:delete,:edit]
 
   # GET /photos
   # GET /photos.json
